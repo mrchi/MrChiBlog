dcd68af25a6e666a734b1470d08e02f9aca5fa36
Invoker <chiqingjun@gmail.com>
2018-07-10 12:57:02 +0800

    添加 shell 内容

--- manage.py
+++ manage.py
@@ -10,3 +10,7 @@ from config import config
 
 app = create_app(config[os.getenv('FLASK_ENV') or 'default'])
 migrate = Migrate(app, db)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db)
