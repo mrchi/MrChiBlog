35b4d8f6deecd3d699b04a5b6d35cc07b7f4ad93
Invoker <chiqingjun@gmail.com>
2018-07-22 22:35:16 +0800

    增加 首页

--- blog/views/main.py
+++ blog/views/main.py
@@ -1,10 +1,28 @@
 # coding=utf-8
 
-from flask import Blueprint
+from flask import Blueprint, render_template, g
+
+from blog.models import Post
+from .common import check_args
 
 bp_main = Blueprint("main", __name__)
 
 
 @bp_main.route("/")
+@check_args("page:?int")
 def index():
-    return "hello, world!"
+    page = g.args.get("page", 1)    # 页码
+    per_page = 10                   # 分页数量
+    pagination = Post.query \
+        .filter_by(status=1) \
+        .order_by(Post.last_update.desc()) \
+        .paginate(page, per_page, error_out=False)
+
+    posts = pagination.items
+
+    return render_template(
+        "index.html",
+        posts=posts,
+        pagination=pagination,
+        endpoint="main.index",
+    )
