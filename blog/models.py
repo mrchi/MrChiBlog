6acfb9b68f77829c89db9430070678475ad6b634
Invoker <chiqingjun@gmail.com>
2018-07-26 18:18:31 +0800

    使用pygments进行代码高亮

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from urllib.parse import urljoin
 
 from markdown import markdown
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.mysql import LONGTEXT
+
 
 db = SQLAlchemy()
 
@@ -26,8 +28,8 @@ class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(128), unique=True, nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    html_content = db.Column(db.Text, nullable=False, default="")
+    content = db.Column(LONGTEXT, nullable=False)
+    html_content = db.Column(LONGTEXT, nullable=False, default="")
     last_update = db.Column(db.DateTime, nullable=False)
     coding_path = db.Column(db.String(256), nullable=False, unique=True)
     status = db.Column(db.Integer, nullable=False, default=0)   # 0:未公开，1:公开，2:删除
@@ -38,15 +40,15 @@ class Post(db.Model):
 def on_changed_md(target, value, oldvalue, initiator):
     """转换 markdown 为 html"""
     extensions = [
-        "markdown.extensions.fenced_code",
-        "markdown.extensions.codehilite",
-        "markdown.extensions.sane_lists",
-        "markdown.extensions.smarty",
-        "markdown.extensions.tables",
-        "markdown.extensions.toc",
+        "markdown.extensions.fenced_code",      # 代码块
+        "markdown.extensions.codehilite",       # 代码块代码高亮
+        "markdown.extensions.sane_lists",       # 非混合列表
+        "markdown.extensions.smarty",           # 自动转译字符的html表示形式
+        "markdown.extensions.tables",           # 表格
+        "markdown.extensions.toc",              # 目录支持
     ]
     extension_configs = {
-        "markdown.extensions.codehilite": {"linenums": True},
+        "markdown.extensions.codehilite": {"linenums": False},  # 不显示行号
     }
     target.html_content = markdown(
         value,
