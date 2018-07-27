065701360c68937ea024223074b3b1832da75577
Invoker <chiqingjun@gmail.com>
2018-07-27 16:00:08 +0800

    增加文章详情页后端部分

--- blog/views/main.py
+++ blog/views/main.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-from flask import Blueprint, render_template, g
+from flask import Blueprint, render_template, g, abort
 
 from blog.models import Post
 from .common import check_args
@@ -11,6 +11,7 @@ bp_main = Blueprint("main", __name__)
 @bp_main.route("/")
 @check_args("page:?int")
 def index():
+    """主页"""
     page = g.args.get("page", 1)    # 页码
     per_page = 10                   # 分页数量
     pagination = Post.query \
@@ -25,4 +26,17 @@ def index():
         posts=posts,
         pagination=pagination,
         endpoint="main.index",
+    )
+
+
+@bp_main.route("/p/<string:permalink>")
+def content(permalink):
+    """文章内容页"""
+    post = Post.query.filter_by(status=1, permalink=permalink).one_or_none()
+    if not post:
+        abort(404)
+
+    return render_template(
+        "post.html",
+        post=post,
     )
