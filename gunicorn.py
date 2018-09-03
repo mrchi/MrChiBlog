1e7ad0b941e92bdc3a422b3eb0c635cfec77a2c3
Invoker <chiqingjun@gmail.com>
2018-09-04 00:53:43 +0800

    移动 gunicorn.py，修改日志配置和绑定地址

--- gunicorn.py
+++ gunicorn.py
@@ -6,7 +6,7 @@
 
 from multiprocessing import cpu_count
 
-bind = ["127.0.0.1:5000", "unix:/tmp/mrchiblog.sock"]   # 绑定监听地址
+bind = ["127.0.0.1:5000"]       # 绑定监听地址
 backlog = 2048                  # pending状态的最大连接数
 
 workers = cpu_count() * 2 + 1   # worker进程数量，推荐值 CPU数量*2+1
@@ -14,8 +14,8 @@ worker_class = "sync"           # worker 工作模式
 threads = 2                     # 每个worker进程的线程数，推荐值范围 [2, 4*cpu数量]
 timeout = 30                    # worker进程超时时间，超时后会重启或杀掉进程
 
-accesslog = "-"                 # 访问日志输出到 stdout
+accesslog = "logs/access.log"   # 访问日志
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
-errorlog = "-"                  # 错误日志输出到 stderr
+errorlog = "logs/error.log"     # 错误日志
 loglevel = "info"               # 错误日志级别
