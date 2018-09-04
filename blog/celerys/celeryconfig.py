1539de67b1613731244191147bcfd063d011c0b4
Invoker <chiqingjun@gmail.com>
2018-09-04 14:53:20 +0800

    docker compose 部署

--- blog/celerys/celeryconfig.py
+++ blog/celerys/celeryconfig.py
@@ -3,6 +3,6 @@
 import os
 
 broker_url = os.environ.get("CELERY_BROKER_URL")
-result_backend = os.environ.get("CELERY_RESULT_BACKED")
+result_backend = os.environ.get("CELERY_RESULT_BACKEND")
 timezone = "Asia/Shanghai"
 task_soft_time_limit = 60
