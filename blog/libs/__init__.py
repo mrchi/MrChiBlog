1abe4abdfb2879ed674894ebcac507d16a23a4da
Invoker <chiqingjun@gmail.com>
2018-12-22 16:37:19 +0800

    增加对 GitHub webhook 的支持

--- blog/libs/__init__.py
+++ blog/libs/__init__.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from .dingtalk import DingTalkRobot
-from .repo import PostRepo, CodingSignature
+from .repo import PostRepo, WebhookSignature
 
 
-__all__ = ["DingTalkRobot", "PostRepo", "CodingSignature"]
+__all__ = ["DingTalkRobot", "PostRepo", "WebhookSignature"]
